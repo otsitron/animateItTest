5a824474945e1eb3ffb5828f4b3059695e3b709c
Olga Tsitron <otsitron@yahoo.com>
2016-10-09 15:50:30 -0400

    html5 canvas test

---
+++ js/animate.js
@@ -0,0 +1,8 @@
+if (document.createElement('canvas').getContext) {
+  console.log("Success! The canvas element is supported.");
+}
+
+window.onload = function () {
+    console.log("window loaded");
+    //Our code here...
+  };
