e531e6a4cb8bef3b14e07682644782571b344e8d
Olga Tsitron <otsitron@yahoo.com>
2017-03-15 23:34:48 -0400

    i don't remember this

--- js/viewModel.js
+++ js/viewModel.js
@@ -1,11 +1,12 @@
-function WebmailViewModel() {
-    // Data
-    var self = this;
-    self.folders = ['Inbox', 'Archive', 'Sent', 'Spam'];
-    self.chosenFolderId = ko.observable();
-
-    // Behaviours    
-    self.goToFolder = function(folder) { self.chosenFolderId(folder); };    
+var SimpleListModel = function(items) {
+    this.items = ko.observableArray(items);
+    this.itemToAdd = ko.observable("");
+    this.addItem = function() {
+        if (this.itemToAdd() != "") {
+            this.items.push(this.itemToAdd()); // Adds the item. Writing to the "items" observableArray causes any associated UI to update.
+            this.itemToAdd(""); // Clears the text box, because it's bound to the "itemToAdd" observable
+        }
+    }.bind(this);  // Ensure that "this" is always this view model
 };
-
-ko.applyBindings(new WebmailViewModel());
+ 
+ko.applyBindings(new SimpleListModel(["Alpha", "Beta", "Gamma"]));
